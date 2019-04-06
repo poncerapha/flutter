e29ef25b08d8dc11e5a418b899f12e9bee1d70cc
poncerapha <raphafleury123@gmail.com>
2019-04-06 12:59:27 -0300

    Adicionando botão

--- app_udemy/lib/main.dart
+++ app_udemy/lib/main.dart
@@ -4,23 +4,46 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+
+class _MyAppState extends State<MyApp>{
+  List<String> _products = ['Food Tester'];
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/food.jpg'),
-              Text('Food Paradise')
-            ],
+          appBar: AppBar(
+            title: Text('EasyList'),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Product'),
+                ),
+              ),
+              Column(children: _products.map((element) => Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/food.jpg'),
+                    Text(element)
+                  ],
+                ),
+              )).toList(),),
+              
+            ],
+          )),
     );
   }
 }
